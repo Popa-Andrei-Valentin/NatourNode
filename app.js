4018e97bae4bb0c383a244fa0bda82d42a3d7c31
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-07-05 20:23:09 +0300

    Installed nodemon and tested

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express()
+
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({
+//       message: 'Hello from the server side',
+//       app: "Natour"
+//     });
+// })
+//
+// app.post("/", (req, res) => {
+//   res.send("You can post here ?")
+// })
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Running on port: ${port}`);
+})
