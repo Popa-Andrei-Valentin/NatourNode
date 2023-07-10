fa1c3ebcfaea3eee7e4de56a9dabb1fda6bf35c6
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-07-10 22:11:39 +0300

    Testing with middleware

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req,res,next) => {
+  console.log("Hello from the middleware")
+  next()
+})
+
+app.use((req,res,next) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
+
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'succes',
     results: tours.length,
