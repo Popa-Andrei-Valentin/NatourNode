fa58a7d982a5e878f2d2bb8487b5f83dffcc2935
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-07-13 21:44:18 +0300

    Created multiple routers for users and tours

--- app.js
+++ app.js
@@ -143,26 +143,32 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 // 3) ROUTES
-app
-  .route('/api/v1/tours')
+const tourRouter = express.Router()
+const userRouter = express.Router()
+
+tourRouter
+  .route('/')
   .get(getAllTours)
   .post(createTour);
 
-app
-  .route('/api/v1/tours/:id')
+tourRouter
+  .route('f/:id')
   .get(getSpecificTour)
   .patch(updateTour);
 
-app
-  .route('/api/v1/users')
+userRouter
+  .route('/')
   .get(getAllUsers)
   .post(createUser);
 
-app
-  .route('/api/v1/users/:id')
+userRouter
+  .route('/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
 // 4) START SERVER
 const port = 3000
