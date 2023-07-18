8b795513d9247078794786a903cb960b9ed3fde5
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-07-18 20:49:58 +0300

    Installed mongoose and set up MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const mongoose = require('mongoose')
 const dotenv = require("dotenv")
 dotenv.config({path: "./config.env"})
 
 const app = require("./app");
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
