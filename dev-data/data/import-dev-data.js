07361086f4f34692290f198591a713209c2853af
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-07-24 22:55:31 +0300

    Created a small script in order to rapidly import or delete all data to/from MongoDB

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+dotenv.config({path: "./config.env"})
+
+const Tour = require("./../../models/tourModels")
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const port = process.env.PORT || 3000;
+
+// READ JSON FILE
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8"));
+
+// IMPORT DATA INTO DATABASE
+const importData = async () => {
+   try {
+     await Tour.create(tours)
+     console.log("Data success fully loaded");
+   } catch (err) {
+     console.log(err)
+   }
+  process.exit()
+
+}
+
+// DELETE ALL DATA FROM DB
+const deleteData = async() => {
+  try {
+    await Tour.deleteMany()
+    console.log("Data deleted from DB ");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit()
+}
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData()
+}
+
+console.log(process.argv)
