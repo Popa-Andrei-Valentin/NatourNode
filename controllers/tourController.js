ad84321a24c24106c63bcf2acbc366d8493d8cff
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-08-03 21:36:12 +0300

    Experimenting with pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,6 +28,22 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select("-__v"); // in this case it excludes the mentioned field.
     }
+
+    // f) Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit)
+
+    if (req.query.page) {
+      // Throw error if the page is non existent.
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');çç
+      }
+    }
+
     // 2. Execute the query.
     const tours = await query
 
