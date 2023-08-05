13a937f27e153ef0b136ee16e37d4f253a177beb
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-08-05 10:58:09 +0300

    Implemented document middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema({
   // Schema definition object.
@@ -7,6 +8,9 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
     trim: true
+  },
+  slug: {
+    type: String
   },
   duration : {
     type: Number,
@@ -69,6 +73,17 @@ tourSchema.virtual("durationWeeks").get(function() {
   return this.duration / 7;
 })
 
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre("save", function(next) {
+  // This middleware is called right before we save the data to the DB.
+  this.slug = slugify(this.name, {lower: true});
+  next();
+});
+
+// tourSchema.post("save", function(doc, next) {
+//   console.log(doc);
+//   next()
+// })
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
