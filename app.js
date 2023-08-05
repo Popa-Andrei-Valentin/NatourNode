69a57140a812446666021f7cde3337bc9585b659
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-08-05 20:41:41 +0300

    Handle unknown route function added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const { json } = require('express');
 
 const app = express();
 
@@ -24,4 +25,11 @@ app.use((req,res,next) => {
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
+// Handle unknown routes.
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status:'fail',
+    message: `Can't find ${req.originalUrl} on this server !`
+  })
+})
 module.exports = app;
