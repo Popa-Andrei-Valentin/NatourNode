fa36fb811655907e344c963581c6ce9abe439b25
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-08-07 23:27:28 +0300

    Refactored the error message for Dev and Prod env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,43 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const sendErrorDev =  (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  })
+}
 
+const sendErrorProduction = (err, res) => {
+  /**
+   * Complex error, not to be sent to the user as is.
+   */
+  if (!err.isOperational) {
+    // 1. Log the error.
+    console.error("ERROR:", err);
+
+    // 2.Send generic message.
+    return res.status(500).json({
+      status: "fail",
+      message: "Ups! Something went wrong, try again..."
+    })
+  }
+
+  /**
+   * Operational error, trusted to be sent to the user as is.
+   */
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message
   })
+}
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProduction(err, res);
+  }
 }
