78b1db9dda7771cbdcb6bef996a13bcc9f92baa7
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-08-28 20:41:44 +0300

    Installed helmet library and added some comments inside app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorController = require("./controllers/errorController")
@@ -12,6 +13,10 @@ const { json } = require('express');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARE
+// SET Security HTTP headers.
+app.use(helmet())
+
+// Development logging.
 if (process.env.NODE_ENV === "development") {
   app.use(morgan('dev'));
 }
@@ -28,9 +33,14 @@ const limiter = rateLimit({
 })
 
 app.use("/api", limiter);
-app.use(express.json());
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware used for debugging and optimisation. TODO: Remove it if not needed anymore.
 app.use((req,res,next) => {
   req.requestTime = new Date().toISOString()
   next()
