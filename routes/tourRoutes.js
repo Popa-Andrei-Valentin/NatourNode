1286e1cb05e09650c309f5ef44deeadb88f95930
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-09-05 20:42:03 +0300

    Experimenting with nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const tourController = require('./../controllers/tourController');
 const authController = require("./../controllers/authController");
+const reviewController = require("./../controllers/reviewController");
 
 const router = express.Router()
 
@@ -28,4 +29,11 @@ router
     authController.restrictTo("admin","lead-guide"),
     tourController.deleteTour);
 
+router
+  .route("/:tourId/reviews")
+  .post(
+    authController.protect,
+    authController.restrictTo("user"),
+    reviewController.createNewReview
+  )
 module.exports = router
