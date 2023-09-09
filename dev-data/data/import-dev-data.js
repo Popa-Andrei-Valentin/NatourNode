11e747917f8e6ee52979e5529b69df6363eaa2f6
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-09-09 11:31:21 +0300

    Updated injection script for User,Reviews and Tours data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv")
 dotenv.config({path: "./config.env"})
 
 const Tour = require("./../../models/tourModels")
+const Review = require("./../../models/reviewModel")
+const User = require("./../../models/userModel")
 
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
@@ -15,12 +17,16 @@ const port = process.env.PORT || 3000;
 
 // READ JSON FILE
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+// const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, "utf-8"));
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
    try {
-     await Tour.create(tours)
+     // await Tour.create(tours)
+     // await Review.create(reviews, { validateBeforeSave: false })
+     await User.create(users, { validateBeforeSave: false })
      console.log("Data success fully loaded");
    } catch (err) {
      console.log(err)
@@ -32,7 +38,9 @@ const importData = async () => {
 // DELETE ALL DATA FROM DB
 const deleteData = async() => {
   try {
-    await Tour.deleteMany()
+    // await Tour.deleteMany()
+    // await Review.deleteMany()
+    await User.deleteMany()
     console.log("Data deleted from DB ");
   } catch (err) {
     console.log(err);
