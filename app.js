8fe4e627ea3256afc83c6ace0b3cfd7547041fcc
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-09-10 18:49:54 +0300

    Installed pug and implemented it

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
@@ -15,6 +16,13 @@ const reviewRouter = require("./routes/reviewRoutes");
 const { json } = require("express");
 
 const app = express();
+
+// PUG Engine setup.
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "./views"));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // 1) GLOBAL MIDDLEWARE
 // SET Security HTTP headers.
@@ -65,9 +73,6 @@ app.use(hpp({
   ]
 }));
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware used for debugging and optimisation. TODO: Remove it if not needed anymore.
 app.use((req,res,next) => {
   req.requestTime = new Date().toISOString()
@@ -75,6 +80,10 @@ app.use((req,res,next) => {
 });
 
 // 2) ROUTES
+app.get("/", (req, res) => {
+  res.status(200).render("base");
+})
+
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
