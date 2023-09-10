13742f13c4efa884376ca13588ee7a3c73b53ed4
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-09-10 17:31:25 +0300

    Calculate distances to Tours from entered point method addeD

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -24,6 +24,8 @@ router.route("/monthly-plan/:year").get(
 
 router.route("/tours-within/:distance/center/:latlng/unit/:unit").get(tourController.getToursWithin );
 
+router.route("/distances/:latlng/unit/:unit").get(tourController.getDistances)
+
 router
   .route('/')
   .get(tourController.getAllTours)
