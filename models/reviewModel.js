9d70cf9c16148bf17fea9d254d30d23be0e47a5e
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-09-10 13:06:36 +0300

    Fixed creating multiple review for one tour by the same user issue and added Math.round for ratingsAverage

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,9 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 })
 
+// Prevent review duplication -> an user can write only one review per tour.
+reviewSchema.index({ tour:1, user:1 }, { unique:true })
+
 reviewSchema.pre(/^find/, function(next){
   this.populate({
     path: "user",
