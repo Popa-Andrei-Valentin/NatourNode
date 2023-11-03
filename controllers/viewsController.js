2a5de78603154d38c3d7a2a03c2ae057fb38b3a3
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-11-03 21:59:38 +0200

    Created overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require("../models/tourModels");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+  console.log(tours)
+  // 3) Render template using the tour data from step #1
   res.status(200).render('overview', {
-    title: "All tours"
+    title: "All tours",
+    tours: tours,
   });
-}
+})
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
