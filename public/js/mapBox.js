94a95f6c8eb66100703b531aed31b373c19dcadc
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-11-27 23:58:53 +0200

    Added pop-up on map for each day of the adventure

--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -3,5 +3,40 @@ const locations = JSON.parse(document.getElementById('map').dataset.locations);
 mapboxgl.accessToken = 'YOUR_MAPBOX_TOKEN';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+  style: 'mapbox://styles/mapbox/streets-v11',
+  scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Add marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  }).setLngLat(loc.coordinates).addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset:30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates)
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top:200,
+    bottom: 200,
+    left: 100,
+    right: 100
+  }
 });
