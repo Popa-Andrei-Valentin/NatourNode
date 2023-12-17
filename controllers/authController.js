5b52fcd2025b7f98d66817841b91899915927ed2
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-12-17 17:51:38 +0200

    Implemented 'Log Out' functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,8 +62,15 @@ exports.login = catchAsync(async (req, res, next) => {
     }
   // 3) If everything ok, send token to client.
   createSendToken(user, 200, res);
-
 })
+
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 10000),
+    httpOnly: true
+  });
+  res.status(200).json({status: "success"})
+}
 
 /** Middleware that ensures user is authenticated **/
 exports.protect = catchAsync(async (req, res, next) => {
@@ -94,25 +101,29 @@ exports.protect = catchAsync(async (req, res, next) => {
 })
 
 /** Middleware that is used only for rendered pages **/
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    // 1) Verify JWT token
-    const decodedData = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
+    try {
+      // 1) Verify JWT token
+      const decodedData = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
 
-    // 2)  Check if users still exists.
-    const currentUser = await User.findById(decodedData.id);
-    if (!currentUser) return next();
+      // 2)  Check if users still exists.
+      const currentUser = await User.findById(decodedData.id);
+      if (!currentUser) return next();
 
-    // 3) Check if user changed password after the JWT token was issued
-    if (currentUser.changedPasswordAfter(decodedData.iat)) {
-      return next( );
+      // 3) Check if user changed password after the JWT token was issued
+      if (currentUser.changedPasswordAfter(decodedData.iat)) {
+        return next();
+      }
+      // 4) Grant access to protected route.
+      res.locals.user = currentUser
+      return next();
+    } catch (err) {
+      return next();
     }
-    // 4) Grant access to protected route.
-    res.locals.user = currentUser
-    return next();
   }
   next()
-})
+}
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
