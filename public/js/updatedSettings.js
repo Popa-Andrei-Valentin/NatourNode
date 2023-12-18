67919d02eed54cceb3b91f4d7c42cc823ad6afb0
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-12-18 17:23:58 +0200

    Implemented update password functionality

--- public/js/updatedSettings.js
+++ public/js/updatedSettings.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+/**
+ * type is either 'password' or 'data'
+ * @param data
+ * @param type
+ * @returns {Promise<void>}
+ */
+export const updateSettings = async (data, type) => {
+  const url = type === "password" ? "updateMyPassword" : "updateMe";
+
   try {
     const res = await axios({
       method: "PATCH",
-      url: `http://127.0.0.1:3000/api/v1/users/updateMe`,
-      data: {
-        name,
-        email
-      }
+      url: `http://127.0.0.1:3000/api/v1/users/${url}`,
+      data
     });
 
-    if (res.data.status === "success") {
-      showAlert("success", "Data updated successfully");
+    if (res.data.status === "succes") {
+      showAlert("success", `${type.toUpperCase()} updated successfully`);
     }
 
   } catch (err) {
