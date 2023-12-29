70e7590827529e408740ebf5b97db701af05c09f
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-12-29 22:55:01 +0200

    Implement email methods

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken")
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError")
-const sendEmail = require("./../utils/email")
+const Email = require("./../utils/email")
 const crypto = require("crypto");
 
 const signToken = id => {
@@ -43,6 +43,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 })
@@ -155,11 +158,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
                             If you didn't forgot your password please ignore this email`
 
     try {
-      await sendEmail({
-        email: user.email,
-        subject: "Your password reset token (valid for 10 min )",
-        message
-      });
+      // await sendEmail({
+      //   email: user.email,
+      //   subject: "Your password reset token (valid for 10 min )",
+      //   message
+      // });
 
       res.status(200).json({
         status: "success",
--- utils/email.js
+++ utils/email.js
@@ -1,35 +1,53 @@
 const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) Create a transporter.
-  //   const transporterOptions = {
-  //     host: process.env.EMAIL_HOST,
-  //     port: process.env.EMAIL_PORT,
-  //     auth: {
-  //       user: process.env.EMAIL_USERNAME,
-  //       password: process.env.EMAIL_PASS
-  //     }
-  //   };
-  let transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 25,
-    auth: {
-      user: "ab15a5093666d2",
-      pass: "36eab8f4714e61"
+// new Email(user, url).sendWelcome();
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Popa Andrei <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
     }
-  });
-    // console.log(transporterOptions, transporter);
-  // 2) Define email options.
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    })
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    })
+    // 2) Define email options
     const mailOptions = {
-      from: "Popa Andrei-Valentin <andrei_popa2009@icloud.com>",
-      to: options.email,
-      subject: options.subject,
-      text: options.message,
-      // html:
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html)
     }
 
-  // 3) Actually send the email.
-    await transport.sendMail(mailOptions);
-}
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
 
-module.exports = sendEmail;
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+};
